68633695d61af7de9afd3bb870d3ec1280661468
joshua-8 <joshua262144@gmail.com>
2024-10-08 00:10:39 -0700

    joystick sticks can move

--- website/script.js
+++ website/script.js
@@ -6,23 +6,27 @@ class DSItem {
 
         this.width = 300;
         this.height = 300;
+        this.radius = 25;
 
         this.x = 0;
         this.y = 0;
-        this.isDragging = false;
 
-        const ctx = this.dsCanvas.getContext('2d');
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.mousePressed = false;
+
+        this.beingEdited = true;
+
+        this.joyx = 0;
+        this.joyy = 0;
 
         this.dsCanvas.addEventListener('mousedown', this.onMouseDown.bind(this));
         this.dsCanvas.addEventListener('mouseup', this.onMouseUp.bind(this));
         this.dsCanvas.addEventListener('mousemove', this.onMouseMove.bind(this));
-
-        // Add touch event listeners
         this.dsCanvas.addEventListener('touchstart', this.onTouchStart.bind(this));
         this.dsCanvas.addEventListener('touchend', this.onTouchEnd.bind(this));
         this.dsCanvas.addEventListener('touchmove', this.onTouchMove.bind(this));
+
+        this.draw();
+
     }
 
     onMouseDown(event) {
@@ -32,7 +36,7 @@ class DSItem {
         if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
             this.offsetX = mouseX - this.x;
             this.offsetY = mouseY - this.y;
-            this.isDragging = true;
+            this.mousePressed = true;
         }
     }
     onTouchStart(event) {
@@ -44,43 +48,60 @@ class DSItem {
             if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
                 this.offsetX = mouseX - this.x;
                 this.offsetY = mouseY - this.y;
-                this.isDragging = true;
+                this.mousePressed = true;
                 this.activeTouchId = touch.identifier;
             }
         }
     }
     onMouseUp() {
-        this.isDragging = false;
+        this.mousePressed = false;
     }
     onMouseMove(event) {
         this.onMove(event.clientX, event.clientY);
     }
     onMove(x, y) {
-        if (this.isDragging) {
-            const rect = this.dsCanvas.getBoundingClientRect();
-            const ctx = this.dsCanvas.getContext('2d');
-            ctx.clearRect(0, 0, this.dsCanvas.width, this.dsCanvas.height);
-            this.x = x - rect.left - this.offsetX;
-            this.y = y - rect.top - this.offsetY;
-            if (this.x < 0) this.x = 0;
-            if (this.y < 0) this.y = 0;
-            if (this.x + this.width > this.dsCanvas.width) {
-                this.x = this.dsCanvas.width - this.width
-            };
-            if (this.y + this.height > this.dsCanvas.height) {
-                this.y = this.dsCanvas.height - this.height
-            };
-            for (let i = 0; i < this.dsItems.length; i++) {
-                this.dsItems[i].draw();
+        if (this.mousePressed) {
+            if (this.beingEdited) {
+                const rect = this.dsCanvas.getBoundingClientRect();
+                this.x = x - rect.left - this.offsetX;
+                this.y = y - rect.top - this.offsetY;
+                this.x = Math.floor(this.x / 10) * 10;
+                this.y = Math.floor(this.y / 10) * 10;
+                if (this.x < 0) this.x = 0;
+                if (this.y < 0) this.y = 0;
+                if (this.x + this.width > this.dsCanvas.width) {
+                    this.x = this.dsCanvas.width - this.width
+                };
+                if (this.y + this.height > this.dsCanvas.height) {
+                    this.y = this.dsCanvas.height - this.height
+                };
+                this.redrawCanvas();
+                this.draw();
+            } else {
+                const rect = this.dsCanvas.getBoundingClientRect();
+                this.joyx = (x - rect.left - this.x - this.width / 2) / (this.width / 2 - this.radius);
+                this.joyy = -(y - rect.top - this.y - this.height / 2) / (this.height / 2 - this.radius);
+                if (this.joyx > 1) this.joyx = 1;
+                if (this.joyx < -1) this.joyx = -1;
+                if (this.joyy > 1) this.joyy = 1;
+                if (this.joyy < -1) this.joyy = -1;
+                console.log(this.joyx, this.joyy);
+                this.draw();
             }
-            this.draw();
+        }
+    }
+    redrawCanvas() {
+        const ctx = this.dsCanvas.getContext('2d');
+        ctx.clearRect(0, 0, this.dsCanvas.width, this.dsCanvas.height);
+        for (let i = 0; i < this.dsItems.length; i++) {
+            this.dsItems[i].draw();
         }
     }
     onTouchEnd(event) {
         for (let i = 0; i < event.changedTouches.length; i++) {
             const touch = event.changedTouches[i];
             if (touch.identifier === this.activeTouchId) {
-                this.isDragging = false;
+                this.mousePressed = false;
                 this.activeTouchId = null;
                 break;
             }
@@ -99,13 +120,22 @@ class DSItem {
 
     draw() {
         const ctx = this.dsCanvas.getContext('2d');
+        ctx.beginPath();
         ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.roundRect(this.x, this.y, this.width, this.height, [this.radius]);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(this.x + this.width / 2 + this.joyx * (this.width / 2 - this.radius), this.y + this.height / 2 - this.joyy * (this.height / 2 - this.radius), this.radius, 0, 2 * Math.PI);
+        ctx.fill();
     }
 }
 
 async function setLed(status) {
-    fetch('./' + (status ? 'on' : 'off'));
+    for (let i = 0; i < DSItems.length; i++) {
+        DSItems[i].beingEdited = status;
+    }
 }
 
 function getAndDownloadData(path) {
